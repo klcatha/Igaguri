88f6ead92f540784eb0aab40f86fd6c15913d70e
klcatha <klcatha0518@gmail.com>
2018-10-26 08:11:39 +0900

    mouse button click igaguri generate

--- Assets/IgaguriGenerator.cs
+++ Assets/IgaguriGenerator.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class IgaguriGenerator : MonoBehaviour {
 
+	public GameObject igaguriPrefab;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,12 @@ public class IgaguriGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject igaguri =
+				Instantiate(igaguriPrefab) as GameObject;
+			igaguri.GetComponent<IgaguriController>().Shoot(
+				new Vector3(0, 200, 2000));
+		}
 	}
 }
